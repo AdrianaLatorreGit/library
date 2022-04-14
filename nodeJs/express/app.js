92defcf8dff11a9dae035066869b71ae1ba403d9
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-04-14 13:48:06 -0300

    Add files via upload

---
+++ nodeJs/express/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+let contato1 = "Maria";
+let contato2 = "Joana";
+let contato3 = "Vanessa";
+app.get("/", (req, res) => res.send("Olá, mundo"));
+app.get("/contato", (req, res) => res.send([contato1, contato2, contato3]));
+app.listen(3033, () => {
+  console.log("Servidor rodando");
+});
