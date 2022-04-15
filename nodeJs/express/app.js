b8fc572b09d49c1135971f7dc53abab38af4ef03
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-04-15 14:44:27 -0300

    Update app.js

--- nodeJs/express/app.js
+++ nodeJs/express/app.js
@@ -8,3 +8,16 @@ app.get("/contato", (req, res) => res.send([contato1, contato2, contato3]));
 app.listen(3033, () => {
   console.log("Servidor rodando");
 });
+
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Retornando tela HOME');
+});
+app.listen(3330, () => {
+  console.log('Iniciando servidor');
+});
