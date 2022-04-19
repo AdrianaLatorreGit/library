463fd4bfcd36935be4e997ad55e5f55afbd3ad11
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-04-18 21:17:40 -0300

    Add files via upload

---
+++ js/calculator/mathOperation.js
@@ -0,0 +1,13 @@
+//export arrow functions
+
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+module.exports = {
+  addition,
+  subtraction,
+  multiplication,
+  division,
+};
