2a94e7db9478c0febc20d8041a4b9f2b5faf5cd3
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-06-06 14:39:13 -0300

    Add files via upload

---
+++ js/orientacaoObjeto/script.js
@@ -0,0 +1,68 @@
+class contaBancaria {
+    constructor(agencia, numero, tipo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    sacar(valor) {
+        if (valor > this._saldo) {
+            return "Operação Negada";
+        }
+        this._saldo = this.saldo - valor;
+
+        return this._saldo;
+    }
+
+    depositar(valor) {
+        this._saldo = this._saldo + valor;
+        return this._saldo;
+    }
+}
+
+class contaCorrente extends contaBancaria {
+    constructor(agencia, numero, cartaoCredito) {
+        super(agencia, numero);
+        this.tipo = "corrente";
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito() {
+        return this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor) {
+        this._cartaoCredito = valor;
+    }
+}
+
+class contaPoupanca extends contaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = "poupança";
+    }
+}
+
+class contaUniversitaria extends contaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = "universitaria";
+    }
+
+    sacar(valor) {
+        if (valor > 500) {
+            return "Operação negada";
+        }
+
+        this._saldo = this._saldo - valor;
+    }
+}
