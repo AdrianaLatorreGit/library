44964654a28688dd46fcb4a3b368880640b9f2a2
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-10-16 11:19:25 -0300

    Add files via upload

---
+++ js/map2/index.js
@@ -0,0 +1,20 @@
+// const numeros = [2, 4, 5, 7];
+
+// const triplo = (array) => {
+//     const triploValores = [];
+//     for (let i = 0; i < array.length; i++) {
+//         triploValores.push(array[i] * 3);
+//     }
+//     return triploValores;
+// };
+
+// const triplo = (array) => {
+//     const triploValores = array.map((numero) => numero * 3);
+//     return triploValores;
+// };
+// console.log(triplo(numeros));
+
+function vezesLetraAparece(frase, letra) {
+    frase.split("");
+    letra.split("");
+}
